e3188d77701566c485f045d26963d7aa29c66abf
gnlsw <today_wsl@163.com>
2018-01-20 14:26:30 +0800

    增加滑窗的算法

--- problem_3/problem_3/src/problem_3.cpp
+++ problem_3/problem_3/src/problem_3.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
+#include <set>
+#include <map>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::set;
+using std::map;
 
 class SolutionA {
 public:
@@ -82,6 +86,77 @@ public:
 	}
 };
 
+class SolutionC {
+public:
+	int lengthOfLongestSubstring(string s)
+	{
+		int max = 0;
+		int i = 0;
+		int j = 0;
+		int len = 0;
+		int n = s.size();
+		set<char> str_set;
+
+		while((i < n) && (j < n))
+		{
+			if(str_set.count(s[j]))
+			{
+				str_set.erase(s[i]);
+				len = j - i;
+				max = max > len ? max : len;
+				i++;
+			}
+			else
+			{
+				str_set.insert(s[j]);
+				j++;
+			}
+		}
+		len = j - i;
+		max = max > len ? max : len;
+
+		return max;
+	}
+};
+
+class SolutionD 
+{
+public:
+	int lengthOfLongestSubstring(string s)
+	{
+		int max = 0;
+		int i = 0;
+		int j = 0;
+		int len = 0;
+		int n = s.size();
+		map<char, int> s_map;
+
+		while((i < n) && (j < n))
+		{
+			if(s_map.find(s[j]) == s_map.end())
+			{
+				s_map[s[j]] = j;
+				j++;
+			}
+			else
+			{
+				if(s_map.find(s[j])->second >= i)
+				{
+					len = j - i;
+					max = max > len ? max : len;
+					i = s_map.find(s[j])->second + 1;
+				}
+				s_map.find(s[j])->second = j;
+				j++;
+			}
+		}
+		len = j - i;
+		max = max > len ? max : len;
+
+		return max;
+	}
+};
+
 int main()
 {
 	string s = "au";
@@ -91,5 +166,11 @@ int main()
 
 	SolutionB sb;
 	max_length = sb.lengthOfLongestSubstring(s);
+
+	SolutionC sc;
+	max_length = sc.lengthOfLongestSubstring("abcabc");
+
+	SolutionD sd;
+	max_length = sd.lengthOfLongestSubstring("abcabcbb");
 	return 0;
 }
